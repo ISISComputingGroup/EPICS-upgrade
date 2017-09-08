acdc82097881465339f9d4e9ecb208e9fee2d0a4
AdrianPotter <adrian.potter@tessella.com>
2017-09-08 16:49:07 +0100

    Add tests for other outcomes

--- test/test_upgrade_step_from_3p2p1p2.py
+++ test/test_upgrade_step_from_3p2p1p2.py
@@ -22,5 +22,37 @@ class TestUpgradeStepFrom3p2p1p2(unittest.TestCase):
         assert_that(result, is_(not_(0)), "result")
         assert_that(self.logger.log_err, has_item("Unable to find the synoptics directory"))
 
+    def test_GIVEN_synoptic_xml_file_has_no_opi_path_WHEN_upgrade_THEN_no_error(self):
+
+        self.file_access.open_xml_file = Mock(return_value=mother.CLEAN_SYNOPTIC_v3p2p1p2)
+
+        result = self.upgrade_step.perform(self.file_access, self.logger)
+
+        assert_that(result, is_(0), "result")
+
+    def test_GIVEN_synoptic_xml_file_has_old_opi_path_WHEN_upgrade_THEN_no_error(self):
+
+        self.file_access.open_xml_file = Mock(return_value=mother.DIRECT_PATH_SYNOPTIC_v3p2p1p2)
+
+        result = self.upgrade_step.perform(self.file_access, self.logger)
+
+        assert_that(result, is_(0), "result")
+
+    def test_GIVEN_synoptic_xml_file_has_new_opi_path_WHEN_upgrade_THEN_no_error(self):
+
+        self.file_access.open_xml_file = Mock(return_value=mother.DIRECT_PATH_SYNOPTIC_v4p0p0)
+
+        result = self.upgrade_step.perform(self.file_access, self.logger)
+
+        assert_that(result, is_(0), "result")
+
+    def test_GIVEN_synoptic_xml_file_has_unknown_opi_path_WHEN_upgrade_THEN_error(self):
+
+        self.file_access.open_xml_file = Mock(return_value=mother.UNKNOWN_SYNOPTIC_v3p2p1p2)
+
+        result = self.upgrade_step.perform(self.file_access, self.logger)
+
+        assert_that(result, is_(not_(0)), "result")
+
 if __name__ == '__main__':
     unittest.main()
