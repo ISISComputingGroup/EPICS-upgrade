0a82aea9d3afdac8f8cf53550a32c687ee25b79a
AdrianPotter <adrian.potter@tessella.com>
2017-09-08 17:24:14 +0100

    Add synoptic opi paths from 2.0.0

--- src/upgrade_step_from_3p2p1p2.py
+++ src/upgrade_step_from_3p2p1p2.py
@@ -4,6 +4,7 @@ from src.common_upgrades.synoptics import Synoptics
 from src.upgrade_step import UpgradeStep
 from data.target_details_release_4p0p0 import OPI_PATH_KEYS as OPI_PATH_KEYS_4P0P0
 from data.target_details_release_3p2p1 import OPI_PATH_KEYS as OPI_PATH_KEYS_3P2P1
+from data.target_details_release_3p2p1 import OPI_PATH_KEYS as OPI_PATH_KEYS_2P0P0
 
 
 class UpgradeStepFrom3p2p1p2(UpgradeStep):
@@ -22,4 +23,5 @@ class UpgradeStepFrom3p2p1p2(UpgradeStep):
         Returns: exit code 0 success; anything else fail
 
         """
-        return Synoptics(file_access, logger).update_opi_paths(dict(OPI_PATH_KEYS_4P0P0, **OPI_PATH_KEYS_3P2P1))
+        return Synoptics(file_access, logger).update_opi_paths(dict(OPI_PATH_KEYS_4P0P0,
+                                                                    **dict(OPI_PATH_KEYS_3P2P1, **OPI_PATH_KEYS_2P0P0)))
