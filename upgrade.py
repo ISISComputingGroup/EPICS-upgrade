aefd3a23a78298c8cce5df95eb752cfb392bfd31
Adrian Potter <Adrian.Potter@tessella.com>
2018-01-24 10:30:06 +0000

    Add upgrade step to version 4.2.0

--- upgrade.py
+++ upgrade.py
@@ -21,7 +21,8 @@ UPGRADE_STEPS = [
     ("3.2.1.2", UpgradeStepFrom3p2p1p2()),
     ("4.0.0", UpgradeStepNoOp()),
     ("4.1.0", UpgradeStepFrom4p1p0()),
-    ("4.1.0.1", None)
+    ("4.1.0.1", UpgradeStepNoOp()),
+    ("4.2.0.0", None),
 ]
 
 if __name__ == "__main__":
