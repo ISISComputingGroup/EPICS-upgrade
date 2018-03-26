4b19b6e8db5651c6cb5036cef47577e29cc91139
John-Holt-Tessella <John.Holt@tessella.com>
2018-03-26 14:11:37 +0100

    Upgrade to 4.3.1

--- upgrade.py
+++ upgrade.py
@@ -23,7 +23,8 @@ UPGRADE_STEPS = [
     ("4.1.0", UpgradeStepFrom4p1p0()),
     ("4.1.0.1", UpgradeStepNoOp()),
     ("4.2.0", UpgradeStepNoOp()),
-    ("4.3.0", None),
+    ("4.3.0", UpgradeStepNoOp()),
+    ("4.3.1", None)
 ]
 
 if __name__ == "__main__":
