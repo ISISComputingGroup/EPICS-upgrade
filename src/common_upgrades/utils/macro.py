73767bb346d36670f9db132e6a0501f43facbd8f
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-07-04 09:39:10 +0100

    Created a utility folder with useful files.

---
+++ src/common_upgrades/utils/macro.py
@@ -0,0 +1,25 @@
+class Macro(object):
+    """
+    Macro Object
+
+    Attributes:
+        ioc_name: Name of the IOC you want to change a macro within
+        name: Macro name. E.g. GALILADDR.
+        value: Value of the Macro. E.g. 1. Defaults to None.
+    """
+
+    def __init__(self, name, value=None):
+        self.__name = name
+        self.__value = value
+
+    def __repr__(self):
+        return '<Macro object with name {} and value {}>'.format(self.__name, self.__value)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def value(self):
+        return self.__value
+
