d614f5a770df124160d4ad9f040af61b10ac27f9
Alistair McGann <alistair.mcgann@tessella.com>
2019-06-12 11:43:46 +0100

    Avoid adding upgrade step before backend code merged

--- upgrade.py
+++ upgrade.py
@@ -35,8 +35,7 @@ UPGRADE_STEPS = [
     ("5.1.1.1", UpgradeMOXA12XXMacros()),
     ("5.2.0", UpgradeStepNoOp()),
     ("5.3.0", UpgradeStepNoOp()),
-    ("5.4.0", ChangeJawsManager()),
-    ("5.4.0.1", None)
+    ("5.4.0", None)
     
     # to add step see https://github.com/ISISComputingGroup/ibex_developers_manual/wiki/Config-Upgrader#adding-an-upgrade-step
 ]
