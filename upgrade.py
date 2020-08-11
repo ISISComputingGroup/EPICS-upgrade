f189d0b43afe2d61743ce3bd41db42f6132dcc1f
Alistair McGann <alistair.mcgann@tessella.com>
2020-08-11 11:29:54 +0100

    Add step for release 7.2

--- upgrade.py
+++ upgrade.py
@@ -54,7 +54,8 @@ UPGRADE_STEPS = [
     ("6.0.0", SetDanfysikDisableAutoonoffMacros()),
     ("6.0.0.1", UpgradeStepNoOp()),
     ("7.0.0", UpgradeStepNoOp()),
-    ("7.1.0", None),
+    ("7.1.0", UpgradeStepNoOp()),
+    ("7.2.0", None)
 
     # to add step see https://github.com/ISISComputingGroup/ibex_developers_manual/wiki/Config-Upgrader#adding-an-upgrade-step
 ]
