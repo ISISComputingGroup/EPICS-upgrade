10a195143b819becadf281d77f340e89d7a5c8e8
JamesKingWork <james.c.king@stfc.ac.uk>
2021-04-22 17:20:12 +0100

    Move to just upgrading IMAT rather than all

--- src/upgrade_step_from_7p4p0.py
+++ src/upgrade_step_from_7p4p0.py
@@ -6,23 +6,17 @@ from src.upgrade_step import UpgradeStep
 
 class SetISOBUSForILM200(UpgradeStep):
     """
-    Set the DISABLE_AUTONOFF macro to true for EMU or add it if not present. When this macro is true,
-    settings will be displayed on the Danfysik OPI allowing automatic power turn on/off.
+    Set the ILM200 ISOBUS value to None for IMAT as they are the first to not use ISOBUS on the ILM200.
     """
-
-    def get_isobus_value_and_add_at_symbol(isobus_value):
-        return f"@{isobus_value}"
 
     def perform(self, file_access, logger):
         try:
             hostname = socket.gethostname()
             # IMAT want it blank as they are not using ISOBUS
-            if hostname != "NDXIMAT":
+            if hostname == "NDXIMAT":
                 ioc_name = "ILM200"
                 change_macros_in_xml = ChangeMacrosInXML(file_access, logger)
-                change_macros_in_xml.add_macro(ioc_name, Macro("ISOBUS", "1"), "^[0-9]+$", "ISOBUS address (default: 1)", "1")
-                get_isobus_value_and_add_at_symbol = lambda isobus_value: f"@{isobus_value}"
-                change_macros_in_xml.manipulate_macro_values(ioc_name, [(Macro("ISOBUS"), get_isobus_value_and_add_at_symbol)])
+                change_macros_in_xml.add_macro(ioc_name, Macro("USE_IOSBUS", "No"), "^(Yes|No)$", "ISOBUS address (default: @1)", "@1")
             return 0
         except Exception as e:
             logger.error("Unable to perform upgrade, caught error: {}".format(e))
