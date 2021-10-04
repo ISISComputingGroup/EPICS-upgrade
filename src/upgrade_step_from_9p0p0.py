00f9fda7408bdb3f19b5034be67d8e746afa173d
Jack Harper <jack.harper@stfc.ac.uk>
2021-10-04 18:07:33 +0100

    adding upgrade step for wish orc

---
+++ src/upgrade_step_from_9p0p0.py
@@ -0,0 +1,18 @@
+import socket
+from src.upgrade_step import UpgradeStep
+
+class ChangeLETCollimatorCmd(UpgradeStep):
+    """
+    Change the LET collimator code to load in the new LET-specific db file.
+    """
+
+    def perform(self, file_access, logger):
+        try:
+            hostname = socket.gethostname()
+            if hostname == "NDXLET":
+                file_access.write_file("configurations\\galil\\oscillatingCollimator.cmd", [r'dbLoadRecords("$(MOTOREXT)/db/oscillatingCollimator_Let.db", "P=$(MYPVPREFIX)MOT:, O=$(COLLIMATOR_PV_PREFIX), M=MTR$(CNUM_PADDED)$(MNUM_PADDED), D=DMC$(CNUM_PADDED), AXIS=$(MOTOR_NUMBER), OTYP=$(OTYP=asynFloat64)")'], mode="a")
+                
+            return 0
+        except Exception as e:
+            logger.error("Unable to perform upgrade, caught error: {}".format(e))
+            return 1
