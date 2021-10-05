91ab7b7da8ff3530db5af6e137f4b08580350b01
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-10-05 11:57:56 +0100

    Added ORC upgrade for MERLIN

--- src/upgrade_step_from_9p0p0.py
+++ src/upgrade_step_from_9p0p0.py
@@ -1,16 +1,17 @@
 import socket
 from src.upgrade_step import UpgradeStep
 
+
 class ChangeLETCollimatorCmd(UpgradeStep):
     """
-    Change the LET collimator code to load in the new LET-specific db file.
+    Change the LET/MERLIN collimator code to load in the new LET/MERLIN-specific db file.
     """
 
     def perform(self, file_access, logger):
         try:
             hostname = socket.gethostname()
-            if hostname == "NDXLET":
-                file_access.write_file("configurations\\galil\\oscillatingCollimator.cmd", [r'dbLoadRecords("$(MOTOREXT)/db/oscillatingCollimator_Let.db", "P=$(MYPVPREFIX)MOT:, O=$(COLLIMATOR_PV_PREFIX), M=MTR$(CNUM_PADDED)$(MNUM_PADDED), D=DMC$(CNUM_PADDED), AXIS=$(MOTOR_NUMBER), OTYP=$(OTYP=asynFloat64)")'], mode="a")
+            if hostname == "NDXLET" or hostname == "NDXMERLIN":
+                file_access.write_file("configurations\\galil\\oscillatingCollimator.cmd", [r'dbLoadRecords("$(MOTOREXT)/db/oscillatingCollimator_Let.db", "P=$(MYPVPREFIX)MOT:, O=$(COLLIMATOR_PV_PREFIX), M=MTR$(CNUM_PADDED)$(MNUM_PADDED), D=DMC$(CNUM_PADDED), AXIS=$(MOTOR_NUMBER), OTYP=$(OTYP=asynFloat64)")\n'], mode="a")
                 
             return 0
         except Exception as e:
