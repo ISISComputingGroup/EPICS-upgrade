41ec92c67bd854bb9151e57d426a4edf848d97b1
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-08-10 01:17:44 +0100

    Add moxa upgrade step

---
+++ src/upgrade_step_from_12p0p3.py
@@ -0,0 +1,22 @@
+import os
+import socket
+
+from src.upgrade_step import UpgradeStep
+from src.common_upgrades.sql_utilities import SqlConnection, run_sql_file
+from src.common_upgrades.utils.constants import EPICS_ROOT
+
+class UpgradeFrom12p0p3(UpgradeStep):
+    """
+    add sql tables for MOXA
+    """
+
+    def perform(self, file_access, logger):
+        # add MOXA Tables
+        try:
+            file = os.path.join(EPICS_ROOT, "SystemSetup", "moxas_mysql_schema.txt")
+            logger.info("Updating moxa schema")
+            with SqlConnection() as s:
+                return run_sql_file(logger, file)
+        except Exception as e:
+            logger.error("Unable to perform upgrade, caught error: {}".format(e))
+            return 1
