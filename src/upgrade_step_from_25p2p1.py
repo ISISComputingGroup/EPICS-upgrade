427fe3be55cfbb85a0d3785f172984a63f9eb876
Jack Harper <jack.harper@stfc.ac.uk>
2025-03-12 15:34:40 +0000

    ruff

--- src/upgrade_step_from_25p2p1.py
+++ src/upgrade_step_from_25p2p1.py
@@ -12,6 +12,7 @@ class UpgradeFrom25p2p1(UpgradeStep):
     """
     Update calibrations repo to use new (gitlab) remote.
     """
+
     def perform(self, file_access: FileAccess, logger: logging.Logger) -> int:
         repo: git.Repo = RepoFactory.get_repo(CALIB_FOLDER)
         repo.remote("origin").set_url("https://gitlab.stfc.ac.uk/isisexperimentcontrols/common.git")
