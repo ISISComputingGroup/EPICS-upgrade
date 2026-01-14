8b066093d86425a3caa19bd069859bbd00117653
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2026-01-14 17:23:24 +0000

    Upgrade to 26.2.0

--- upgrade.py
+++ upgrade.py
@@ -76,7 +76,8 @@ UPGRADE_STEPS = [
     ("25.2.1", UpgradeFrom25p2p1()),
     ("25.2.1.1", UpgradeFrom25p2p1p1()),
     ("25.2.2", UpgradeStepNoOp()),
-    ("25.8.0", None),
+    ("25.8.0", UpgradeStepNoOp()),
+    ("26.2.0", None),
     # to add step see README.md
     ## Do not consider dropping the previous last entry even if adding a new step that does nothing.
     ## Though that version may not have been deployed to any instruments, the config version will exist
